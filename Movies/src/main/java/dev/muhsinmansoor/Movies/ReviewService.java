a44de3784bfcbc18c940026f01791c5cd5162c28
Muhsin Mansoor <muhsinmansoor7@gmail.con>
2023-08-31 21:25:48 +0530

    This is my third commit. Backend completed.

--- Movies/src/main/java/dev/muhsinmansoor/Movies/ReviewService.java
+++ Movies/src/main/java/dev/muhsinmansoor/Movies/ReviewService.java
@@ -1,10 +1,13 @@
 package dev.muhsinmansoor.Movies;
+//package dev.muhsinmansoor.Movies.ReviewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
@@ -16,7 +19,6 @@ public class ReviewService {
         Review review = reviewRepository.insert(new Review(reviewBody));
 
         // insert it to database
-
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
